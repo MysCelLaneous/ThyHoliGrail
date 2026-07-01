41f44b3f0dda10ed4530158619df68a8893188c7
MysCelLaneous <college_fresh06@yahoo.com>
2026-07-01 12:40:34 -0500

    Add event listener to close navigation on link click

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const nav = document.getElementById("nav");
 menuBtn.addEventListener("click", () => {
   nav.classList.toggle("active");
 });
+
+document.querySelectorAll(".nav a").forEach((link) => {
+  link.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
+});
